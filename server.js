ac9c3858c7944ea22a187c1afaa56965748ff513
kdd195 <kdd195@msstate.edu>
2017-11-27 14:14:11 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 // server.js
+var cors = require('cors');
 var app = require('./app');
-var port = process.env.PORT || 3000;
+//var port = process.env.PORT || 3000;
 
-var server = app.listen(port, function() {
-  console.log('Express server listening on port ' + port);
+app.use(cors());
+
+var server = app.listen(80, function() {
+  console.log('Express server listening on port 80');
 });
